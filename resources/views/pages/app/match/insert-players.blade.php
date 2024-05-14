487ff1f95315432f217279bcf060b1688e6d3933
Vincent Billerey <vincent.billerey.web@gmail.com>
2024-05-14 11:50:27 +0200

    Ajout du bouton valider dans insert players

--- resources/views/pages/app/match/insert-players.blade.php
+++ resources/views/pages/app/match/insert-players.blade.php
@@ -97,6 +97,9 @@
                         </tr>
                     </tbody>
                 </table>
+                <div class="p-6 text-center w-full text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+                    <button type="button" class="text-white w-64 bg-gradient-to-r  from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Valider</button>
+                </div>
             </div>
         </div>
       </section>
